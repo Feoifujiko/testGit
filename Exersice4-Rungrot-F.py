3ae95cdda850fc191cffa92139b6a6a9f538d618
Feoifujiko <feoiprogramming@gmail.com>
2020-05-20 10:19:22 +0700

    finish

---
+++ Exersice4-Rungrot-F.py
@@ -0,0 +1,11 @@
+print(".....Your score.....")
+
+FE = 67
+GB = 55
+ICS = 77
+CP = 88
+
+print("Foundation English is :", FE)
+print("General Business is :", GB)
+print("Introduction to Computer Systems is :", ICS)
+print("Computer Programming is :", CP)
